4fe87cc8635cb6b680edc490b827b651a608dbda
Danil Postvaikin <postvaykin01@mail.ru>
2020-09-27 20:20:11 +0300

    fef

---
+++ cmake-build-debug/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/sources/JSON.cpp.o"
+  "CMakeFiles/parser.dir/sources/Students.cpp.o"
+  "libparserd.a"
+  "libparserd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/Students.hpp
@@ -0,0 +1,10 @@
+//
+// Created by danil on 27.09.2020.
+//
+
+#ifndef TEMPLATE_STUDENTS_HPP
+#define TEMPLATE_STUDENTS_HPP
+
+class Students {};
+
+#endif  // TEMPLATE_STUDENTS_HPP
